8d109f179f19e0d7e57df9f73c021e45e7c91eee
Tanishq4224 <tanishq4224@gmail.com>
2025-06-06 22:07:00 +0530

    added js functionalities

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+window.onload=()=>{
+let addBtn = document.getElementById("add-btn");
+let bookForm = document.querySelector(".add-book");
+let container = document.querySelector(".container");
 
+
+bookForm.classList.add("hide");
+//toggles hide form and blur the container when add btn is pressed
+addBtn.addEventListener("click" , function(){
+bookForm.classList.toggle("hide");
+container.classList.toggle("blur");
+})
+}
